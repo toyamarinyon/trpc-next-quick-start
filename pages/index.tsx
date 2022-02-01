daeefe77524ae78581820224db6ebc66103091de
toyama satoshi <toyamarinyon@gmail.com>
2022-02-01 10:48:13 +0900

    update design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,22 @@
 // import { trpc } from "../utils/trpc";
 import type { NextPage } from "next";
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
 
 const dummyData = [
   {
     id: 1,
-    title: "dummy a",
+    title: "This post is dummy content",
   },
   {
     id: 2,
-    title: "dummy b",
+    title: "Let's create tRPC server and use it!",
   },
 ];
 const Home: NextPage = () => {
+  const [title, setTitle] = useState();
+  const [filter, setFilter] = useState();
+
   // You should type bellow code in step 4 on walk through.
   // We're highly recommended typing below code instead of copying it.
   // const query = trpc.useQuery(["posts", {}]);
@@ -22,7 +26,28 @@ const Home: NextPage = () => {
         <header className={styles.title}>
           <h1>tRPC with Next.js Quick Demo 🚀</h1>
         </header>
-        <h2>Posts</h2>
+        <section className={styles.controller}>
+          <form className={styles.filter}>
+            <input type="text" id="filter" />
+            <button type="submit">🔍</button>
+          </form>
+          <details>
+            <summary>
+              ✏️
+              <div className={styles.modalOverlay}></div>
+            </summary>
+            <section className={styles.modal}>
+              <div className={styles.modalContent}>
+                <h3>New Post</h3>
+                <form>
+                  <textarea id="postTitle"></textarea>
+                  <button type="submit">Create</button>
+                </form>
+              </div>
+            </section>
+          </details>
+        </section>
+
         <section className={styles.grid}>
           {dummyData.map((data, i) => (
             <article key={`article-${i}`} className={styles.card}>
@@ -30,11 +55,6 @@ const Home: NextPage = () => {
             </article>
           ))}
         </section>
-        <h2>Add post</h2>
-        <form className={styles.form}>
-          <textarea></textarea>
-          <button>submit</button>
-        </form>
       </main>
     </section>
   );
